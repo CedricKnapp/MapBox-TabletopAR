4b683efb0e05f84ea8956cf2e3bc3a97ace3781d
CedricKnapp_CRC <Cedric.Knapp2@canada.ca>
2018-09-17 16:41:15 -0400

    Modified tank orientation behaviour, might have introduced memory leak... oops!

--- MapBox-TabletopAR/Assets/Scripts/Tank.cs
+++ MapBox-TabletopAR/Assets/Scripts/Tank.cs
@@ -9,11 +9,18 @@ public class Tank : MonoBehaviour {
     [SerializeField] private float fireRate = 5.0f;
     [SerializeField] private float shotForce = 1500.0f;
     [SerializeField] private float moveSpeed = 0.2f;
-    [SerializeField] private float travelTime = 1.0f;
+    [SerializeField] private float travelTime = 0.01f;
     [SerializeField] private float killHeight = -10.0f;
 
+    [SerializeField] private int ROTATION_STEPS = 8; //Num of rotation steps in each direction 
+    private int rotateCounter;
+    private RotationDir randRotDir = RotationDir.None;
+
+    enum RotationDir {Clockwise, CounterClockwise, None}
 
     void Start () {
+        rotateCounter = ROTATION_STEPS;
+
         StartCoroutine(FireProjectile());
         StartCoroutine(Move());
     }
@@ -55,9 +62,37 @@ public class Tank : MonoBehaviour {
             transform.Translate(transform.forward * -1 * moveSpeed * Time.deltaTime);
             yield return null;
         }
-        transform.Rotate(rotationAxis, Random.Range(0, 360));
+
+        //transform.Rotate(rotationAxis, Random.Range(0, 360));
+
+        if (randRotDir == RotationDir.Clockwise) {
+            transform.Rotate(rotationAxis,
+                Mathf.Max(transform.eulerAngles.z + Random.Range(3, 7), -360));
+        } else if (randRotDir == RotationDir.CounterClockwise) {   
+            transform.Rotate(rotationAxis,
+                Mathf.Min(transform.eulerAngles.z - Random.Range(3, 7), 360));
+        }
+
+        //Regenerate the rotation direction
+        if (rotateCounter <= 0) {
+            randRotDir = GenerateRandRotNum();
+            rotateCounter = ROTATION_STEPS;
+        }
+
+        rotateCounter--;
         StartCoroutine(Move());
     }
 
-
+    private RotationDir GenerateRandRotNum() {
+        switch(Random.Range(0, 3)) {
+            case 0:
+                return RotationDir.None;
+            case 1:
+                return RotationDir.Clockwise;
+            case 2:
+                return RotationDir.CounterClockwise;
+            default:
+                return RotationDir.None;
+        }
+    }
 }
